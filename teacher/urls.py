6a37d8f3aa3355792f97a483a8bd87e61cdcf229
omid <omizare57@gmail.com>
2023-11-16 16:41:33 +0330

    Add logout view

--- teacher/urls.py
+++ teacher/urls.py
@@ -6,5 +6,4 @@ from . import views
 app_name = 'teacher'
 urlpatterns = [
     path('register/', views.TeacherRegisterView.as_view(), name='register'),
-    path('login/', views.TeacherLoginView.as_view(), name='login'),
 ]
--- user/views.py
+++ user/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.utils.translation import gettext as _
 
-# Create your views here.
+from rest_framework import status
+from rest_framework.generics import GenericAPIView
+from rest_framework.request import Request
+from rest_framework.response import Response
+
+from . import serializers
+
+
+class UserLogoutView(GenericAPIView):
+    serializer_class = serializers.UserLogoutSerializer
+
+    def post(self, request: Request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        return Response({'msg': _('logout OK.')}, status.HTTP_200_OK)
+
+
+class UserLoginView(GenericAPIView):
+    serializer_class = serializers.UserLoginSerializer
+
+    def post(self, request: Request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        return Response(serializer.data)
