f3f604b3a90e7f0e2169be45cc4b1f5980428660
omid <omizare57@gmail.com>
2023-11-16 15:42:09 +0330

    Add teacher login view

--- teacher/urls.py
+++ teacher/urls.py
@@ -6,4 +6,5 @@ from . import views
 app_name = 'teacher'
 urlpatterns = [
     path('register/', views.TeacherRegisterView.as_view(), name='register'),
+    path('login/', views.TeacherLoginView.as_view(), name='login'),
 ]
--- teacher/views.py
+++ teacher/views.py
@@ -1,4 +1,6 @@
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, GenericAPIView
+from rest_framework.request import Request
+from rest_framework.response import Response
 
 from . import serializers
 from user.models import User
@@ -7,3 +9,12 @@ from user.models import User
 class TeacherRegisterView(CreateAPIView):
     queryset = User.objects.all()
     serializer_class = serializers.TeacherRegisterSerializer
+
+
+class TeacherLoginView(GenericAPIView):
+    serializer_class = serializers.TeacherLoginSerializer
+
+    def post(self, request: Request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        return Response(serializer.data)
